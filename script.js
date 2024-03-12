2b4f3629d5c762499cd24fedd9af42635df383ff
Maks Haiduchok <145449886+JuMp1X@users.noreply.github.com>
2024-03-12 14:57:24 +0200

    Code

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+let score = 20;
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+let highscore = document.querySelector('.highscore').textContent;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  //Коли введене значення не є числом
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number⛔!';
+    //Введене значення співпадає з загаданим
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number🏆';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+    //значення більше
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    //значення менше
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
